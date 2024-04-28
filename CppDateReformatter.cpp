d230d3989bd4cd6165d3d12f6a1d7cdc178ea242
mht83 <54060547+mht83@users.noreply.github.com>
2024-04-28 16:01:56 +0330

    Update CppDateReformatter.cpp

--- CppDateReformatter.cpp
+++ CppDateReformatter.cpp
@@ -14,7 +14,7 @@ std::string findAndConvertDates(const std::string text) {
     int pos = 0;
    
     while ((pos = result.find_first_of(".-", pos)) != std::string::npos) {
-        if (pos > 0 && pos < result.length() - 10 && (result[pos + 3] == '.' || result[pos + 3] == '-') && isdigit(result[pos - 1]) && isdigit(result[pos + 4])) {
+        if ((result[pos + 3] == '.' || result[pos + 3] == '-') && isdigit(result[pos - 1]) && isdigit(result[pos + 4])) {
             
             std::string date = result.substr(pos - 2, 10);
             
